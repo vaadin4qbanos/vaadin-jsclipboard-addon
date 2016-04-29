9583fd8472c2eb9f9e3c4bc62a620d16e2f91189
Geanny Hernández Rodríguez <geanny.hernandez@datys.cu>
2016-04-29 17:29:29 -0400

    Cambiando la biblioteca clipboard.js por clipboard.min.js

--- jsclipboard/src/main/java/com/vaadin/jsclipboard/ClipboardButton.java
+++ jsclipboard/src/main/java/com/vaadin/jsclipboard/ClipboardButton.java
@@ -29,7 +29,7 @@ import java.util.UUID;
  *
  * @author Geanny Hernández Rodríguez
  */
-@JavaScript({"bower_components/clipboard/dist/clipboard.js", "clipboard_button.js"})
+@JavaScript({"bower_components/clipboard/dist/clipboard.min.js", "clipboard_button.js"})
 public class ClipboardButton extends AbstractJavaScriptComponent {
 
     private static final long serialVersionUID = 1L;
